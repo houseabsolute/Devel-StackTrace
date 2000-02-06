9dff1997945e3208cde87e3b263f1f918c241b90
autarch <devnull@localhost>
2000-02-06 07:28:47 +0000

    working

---
+++ StackTrace.pm
@@ -0,0 +1,188 @@
+package StackTrace;
+
+use strict;
+use vars qw($VERSION);
+
+use fields qw( index frames );
+
+$VERSION = '0.01';
+
+1;
+
+sub new
+{
+    my $proto = shift;
+    my $class = ref $proto || $proto;
+
+    my $self;
+    {
+	no strict 'refs';
+	$self = bless [ \%{"${class}::FIELDS"} ], $class;
+    }
+
+    $self->{index} = undef;
+    $self->{frames} = [];
+    $self->_add_frames(@_);
+
+    return $self;
+}
+
+sub _add_frames
+{
+    my StackTrace $self = shift;
+    my %p = @_;
+
+    my (%i_pack, %i_class);
+    if ($p{ignore_package})
+    {
+	$p{ignore_package} = [$p{ignore_package}] unless ref $p{ignore_package};
+	%i_pack = map {$_ => 1} @{ $p{ignore_package} };
+    }
+
+    if ($p{ignore_class})
+    {
+	$p{ignore_class} = [$p{ignore_class}] unless ref $p{ignore_class};
+	%i_class = map {$_ => 1} @{ $p{ignore_class} };
+    }
+    # This _will_ cause all subclasses of this class to be ignored as
+    # well.
+    my $p = __PACKAGE__;
+    $i_class{$p} = 1;
+
+    my $x = 0;
+    my @c;
+    while ( do { package DB; @c = caller($x++) } )
+    {
+	# Do the quickest ones first.
+	next if ( exists $p{ignore_package} &&
+		  $i_pack{ $c[0] } );
+	next if ( grep { $c[0]->isa($_) } keys %i_class );
+
+	# eval and is_require are only returned when applicable.
+	push @c, (undef, undef) if scalar @c == 6;
+
+	my @a = @DB::args;
+	push @{ $self->{frames} }, StackTraceFrame->new(@c, \@a);
+    }
+}
+
+sub next_frame
+{
+    my StackTrace $self = shift;
+
+    # reset to top if necessary.
+    $self->{index} = -1 unless defined $self->{index};
+
+    if (defined $self->{frames}[ $self->{index} + 1 ])
+    {
+	return $self->{frames}[ ++$self->{index} ];
+    }
+    else
+    {
+	$self->{index} = undef;
+	return undef;
+    }
+}
+
+sub prev_frame
+{
+    my StackTrace $self = shift;
+
+    # reset to top if necessary.
+    $self->{index} = scalar @{ $self->{frames} } unless defined $self->{index};
+
+    if (defined $self->{frames}[ $self->{index} - 1 ] && $self->{index} >= 1)
+    {
+	return $self->{frames}[ --$self->{index} ];
+    }
+    else
+    {
+	$self->{index} = undef;
+	return undef;
+    }
+}
+
+sub as_string
+{
+    my StackTrace $self = shift;
+
+    my $st = '';
+    foreach my $f (@{ $self->{frames} })
+    {
+	$st .= $f->as_string . "\n";
+    }
+
+    return $st;
+}
+
+package StackTraceFrame;
+
+use strict;
+use vars qw($VERSION);
+
+use fields qw( package filename line subroutine hasargs wantarray evaltext is_require args );
+
+$VERSION = '0.01';
+
+# Create accessor routines
+{
+    no strict 'refs';
+    foreach my $f (keys %{__PACKAGE__.'::FIELDS'})
+    {
+	*{$f} = sub { my StackTraceFrame $s = shift; return $s->{$f} };
+    }
+}
+
+1;
+
+sub new
+{
+    my $proto = shift;
+    my $class = ref $proto || $proto;
+
+    my $self;
+    {
+	no strict 'refs';
+	$self = bless [ \%{"${class}::FIELDS"} ], $class;
+    }
+
+    @{ $self }{ qw( package filename line subroutine hasargs wantarray evaltext is_require args ) } = @_;
+
+    return $self;
+}
+
+sub as_string
+{
+    my StackTraceFrame $self = shift;
+
+    return $self->subroutine . ' called at ' . $self->filename . ' line ' . $self->line;
+}
+
+__END__
+
+=head1 NAME
+
+StackTrace - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use StackTrace;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for StackTrace was created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+A. U. Thor, a.u.thor@a.galaxy.far.far.away
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
---
+++ test.pl
@@ -0,0 +1,136 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+######################### We start with some black magic to print on failure.
+
+# Change 1..1 below to 1..last_test_to_print .
+# (It may become useful if the test is moved to ./t subdirectory.)
+
+BEGIN { $| = 1; print "1..1\n"; }
+END {print "not ok 1\n" unless $main::loaded;}
+use StackTrace;
+use strict
+
+$^W = 1;
+$main::loaded = 1;
+
+result($loaded, "Unable to load StackTrace module\n");
+
+# 2-10 Test all accessors
+{
+    my $trace = foo();
+
+    my @f = ();
+    while ( my $f = $trace->prev_frame ) { push @f, $f; }
+
+    my $cnt = scalar @f;
+    result( $cnt == 4, "Trace should have 4 frames but it has $cnt\n" );
+
+    @f = ();
+    while ( my $f = $trace->next_frame ) { push @f, $f; }
+
+    $cnt = scalar @f;
+    result( $cnt == 4, "Trace should have 4 frames but it has $cnt\n" );
+
+    result( $f[0]->package eq 'main', "First frame package should be main but it's ",
+	    $f[0]->package, "\n" );
+
+    result( $f[0]->filename eq 'test.pl', "First frame package should be test.pl but it's ",
+	    $f[0]->filename, "\n" );
+
+    result( $f[0]->line == 99, "First frame line should be 99 but it's ",
+	    $f[0]->line, "\n" );
+
+    result( $f[0]->subroutine eq 'StackTrace::new', "First frame subroutine should be StackTrace::new but it's ",
+	    $f[0]->subroutine, "\n" );
+
+    result( $f[0]->hasargs == 1, "First frame hasargs should be true but it's not\n" );
+
+    result( $f[0]->wantarray == 0, "First frame wantarray should be false but it's not\n" );
+
+    my $trace_text = <<'EOF';
+StackTrace::new called at test.pl line 99
+main::baz called at test.pl line 94
+main::bar called at test.pl line 89
+main::foo called at test.pl line 21
+EOF
+
+    result( $trace->as_string eq $trace_text,
+	    "Trace should be:\n$trace_text but it's\n", $trace->as_string );
+}
+
+# 11-14 Test constructor params
+{
+    my $trace = SubTest::foo( ignore_class => 'Test' );
+
+    my @f = ();
+    while ( my $f = $trace->prev_frame ) { push @f, $f; }
+
+    my $cnt = scalar @f;
+
+    result( $cnt == 1, "Trace should have 1 frames but it has $cnt\n" );
+    result( $f[0]->package eq 'main',
+	    "The package for this frame should be main but it's ", $f[0]->package, "\n" );
+
+    $trace = Test::foo( ignore_class => 'Test' );
+
+    @f = ();
+    while ( my $f = $trace->prev_frame ) { push @f, $f; }
+
+    $cnt = scalar @f;
+
+    result( $cnt == 1, "Trace should have 1 frames but it has $cnt\n" );
+    result( $f[0]->package eq 'main',
+	    "The package for this frame should be main but it's ", $f[0]->package, "\n" );
+}
+
+sub foo
+{
+    bar(@_, 1);
+}
+
+sub bar
+{
+    baz(@_, 2);
+}
+
+sub baz
+{
+    StackTrace->new( @_[0,1] );
+}
+
+sub result
+{
+    my $ok = !!shift;
+    use vars qw($TESTNUM);
+    $TESTNUM++;
+    print "not "x!$ok, "ok $TESTNUM\n";
+    print @_ if !$ok;
+}
+
+package Test;
+
+sub foo
+{
+    trace(@_);
+}
+
+sub trace
+{
+    StackTrace->new(@_);
+}
+
+package SubTest;
+
+use base qw(Test);
+
+sub foo
+{
+    trace(@_);
+}
+
+sub trace
+{
+    StackTrace->new(@_);
+}
+
