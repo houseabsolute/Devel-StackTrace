b9d65000caf206983f2ca754fb0b0eb660d27de8
Dave Rolsky <autarch@urth.org>
2016-03-02 11:22:27 -0600

    Bump version after release

--- lib/Devel/StackTrace.pm
+++ lib/Devel/StackTrace.pm
@@ -5,7 +5,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '2.01';
+our $VERSION = '2.02';
 
 use Devel::StackTrace::Frame;
 use File::Spec;
--- lib/Devel/StackTrace/Frame.pm
+++ lib/Devel/StackTrace/Frame.pm
@@ -3,7 +3,7 @@ package Devel::StackTrace::Frame;
 use strict;
 use warnings;
 
-our $VERSION = '2.01';
+our $VERSION = '2.02';
 
 # Create accessor routines
 BEGIN {
